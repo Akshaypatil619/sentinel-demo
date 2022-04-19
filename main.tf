9416e47afcf31e93d1ae2a123a6df4ef30f6f13a
Akshaypatil619 <47349096+Akshaypatil619@users.noreply.github.com>
2022-04-19 20:49:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "ubuntu" {
 
 resource "aws_instance" "ubuntu2" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          =  "c5.large"
+  instance_type          =  "t2.micro"
 
   tags = {
     Name = "pimco-test-instance"
